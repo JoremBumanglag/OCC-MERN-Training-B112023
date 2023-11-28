f4838943a97f38d1fb28e68bc7cd09be4334293d
Jorem Bumanglag <bumanglagjorem@gmail.com>
2023-11-28 15:37:18 +0800

    second commit

---
+++ Bumanglag, Jorem/Week_2/Day_1/app.js
@@ -0,0 +1,49 @@
+// let varOne = 1;
+// console.log(varOne);
+
+// varOne = 2;
+// console.log(varOne);
+
+// const my_name = 'Jorem';
+// console.log(my_name);
+
+// //String
+// const string_dataType = 'I am a String';
+// console.log(string_dataType);
+
+// //Integer
+// const int_dataType = 2;
+// console.log(int_dataType)
+
+// //Float
+// const float_dataType = 2.212;
+// console.log(float_dataType)
+
+// //Boolean Value
+// const truthy = true;
+// const falsy = false;
+
+// //Undefined
+// let variable;
+// console.log(variable);
+
+// //Null
+// const nothing = null;
+// console.log(null);
+
+// //Array
+// const fruits = ["apple", "orange", "starapple"];
+// console.log(fruits);
+// console.log(fruits[2]);
+
+//Arithmetic Operators
+const numOne = 5;
+const numTwo = 6;
+
+let result = numOne + numTwo;
+
+console.log(numOne + numTwo);
+console.log(numOne - numTwo);
+console.log(numOne * numTwo);
+console.log(numOne / numTwo);
+console.log(numOne % numTwo);
